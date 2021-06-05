5625a077d433b4fce6f193902ecbc147efb32bae
pavan7710 <bollinenipavan24111997@gmail.com>
2021-06-05 13:59:41 +0530

    bootstrap row and col was added

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -1,25 +1,42 @@
-import React , {useEffect} from 'react'
+import React , {useEffect , Fragment} from 'react'
 import {connect} from 'react-redux'
 import { getAllUser } from '../actions/actions'
-
-const AllUsers = ({getAllUser , users }) => {
-   // console.log(users)
+import Spinner from '../layout/Spinner'
+import Usercard from './Usercard'
+const AllUsers = ({getAllUser , users : {loading , data} }) => {
+    //({users : {loading , data}})
+ //  console.log(users)
+  console.log(loading, data)
+  console.log(data)
     useEffect(()=>{
         getAllUser()
     }, [getAllUser])
 
     return (
-        <div>
-            <p>pavan</p>
-        </div>
+     <Fragment>
+         {loading ? (<Fragment>
+             <Spinner/>
+         </Fragment>) : (
+             <div className="row">
+
+                 {data.map((user) => {
+                     return(
+                         <div  className="col-lg-4 col-md-3">
+                         <Usercard key={user.id} user={user}/>
+                         </div>
+                     )
+
+                 })}
+            </div>
+         )}
+     </Fragment>
     )
 }
 
 const mapStateToProps = (state) => {
-   // console.log(state)
-   // console.log(state.userReducer.data)
+ //   console.log(state.userReducer.data)
     return{
-        users : state.userReducer.data
+        users : state.userReducer
     }
 }
 
--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Usercard = () => {
+const Usercard = ({user}) => {
+
+  
     return (
-        <div>
-            
+     <div className="container">
+           <div className="card" >
+            <img className="card-img-top" src={user.avatar} alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">FirstName:{user.first_name}</h5>
+                <h5 className="card-title">LastName:{user.last_name}</h5>
+                <h5 className="card-title">Email:{user.email}</h5>
+            </div>
         </div>
+
+     </div>
     )
 }
 
